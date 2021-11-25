94a3d0c6618fa62453a1ee8d609c1c709e8ebe64
Pavel <pavelrudeniaaa@gmail.com>
2021-11-25 09:14:02 +0300

    scoreboard

--- venv/Alien_Invasion/button.py
+++ venv/Alien_Invasion/button.py
@@ -5,7 +5,7 @@ class Button():
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         # Назначение размеров и свойств кнопок.
-        self.width, self.height = 200, 50
+        self.width, self.height = 200, 70
         self.button_color = (0, 255, 0)
         self.text_color = (255, 0, 0)
         self.font = pygame.font.SysFont('algerian', 44)
--- venv/Alien_Invasion/ship.py
+++ venv/Alien_Invasion/ship.py
@@ -1,10 +1,11 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
     """класс для управления кораблем"""
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
-
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
